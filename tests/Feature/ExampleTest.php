76a5580809dee2a2db18ba414b8dfa02684e3834
ucan-lab <35098175+ucan-lab@users.noreply.github.com>
2024-10-14 21:31:27 +0900

    phpunit から pest へ

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,6 @@
 
 declare(strict_types=1);
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-final class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+test('The application returns a successful response', function (): void {
+    $this->assertTrue(true);
+});
